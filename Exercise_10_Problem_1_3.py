3ecb25d66494f6ad1929a88dd3d5581b0ee456ae
mayusagawa <81971239+mayu-sagawa@users.noreply.github.com>
2021-07-04 06:19:20 +0000

    I created lines to create a spatial join between grid layer and buffer layer.

--- Exercise_10_Problem_1_3.py
+++ Exercise_10_Problem_1_3.py
@@ -121,7 +121,10 @@ print(pop.head(3))
 
 # Create a spatial join between grid layer and buffer layer. 
 # YOUR CDOE HERE 10 for spatial join
-
+join=gpd.sjoin(geodata,pop,how="inner", op="intersects")
+tokyu=join.loc[join["name"]=="Tokyu Department Store"]
+seibu=join.loc[join["name"]=="Seibu Shibuya Store"]
+national=join.loc[join["name"]=="National Azabu"]
 
 # YOUR CODE HERE 11 to report how many people live within 1.5 km distance from each shopping center
 
