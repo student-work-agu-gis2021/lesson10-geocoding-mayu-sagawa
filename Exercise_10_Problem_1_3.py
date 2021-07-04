62e8687c872c1c4921aafb4ceba7c8b2717ed83b
mayusagawa <81971239+mayu-sagawa@users.noreply.github.com>
2021-07-04 04:40:12 +0000

    I made a line to read the created shopping_centers.txt.

--- Exercise_10_Problem_1_3.py
+++ Exercise_10_Problem_1_3.py
@@ -12,6 +12,9 @@ import pandas as pd
 # Read the data (replace "None" with your own code)
 data = None
 # YOUR CODE HERE 1 to read the data
+data=pd.read_table('shopping_centers.txt',sep=';',header=None)
+data.index.name='id'
+data.columns=['name','addr']
 
 #TEST COEE
 # Check your input data
